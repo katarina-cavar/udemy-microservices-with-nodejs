bee8203cfd837829b24b659f34828da0c110c3d1
Katarina Cavar <katarina.cavar@cloudsensesolutions.com>
2022-09-01 19:38:04 +0200

    Lesson 52: Successful

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,12 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
     const event = req.body;
+
+    events.push(event);
 
     axios.post('http://localhost:4000/events', event).catch((err) => console.log(err));
     axios.post('http://localhost:4001/events', event).catch((err) => console.log(err));
@@ -14,6 +18,10 @@ app.post('/events', (req, res) => {
     axios.post('http://localhost:4003/events', event).catch((err) => console.log(err));
 
     res.send({status: 'OK'});
+});
+
+app.get('/events', (req, res) => {
+    res.send(events);
 });
 
 app.listen(4005, () => {
